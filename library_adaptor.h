a5e5f150c38ff791353fd8a3bacecc8a3b55a39d
Evan Teran <evan.teran@gmail.com>
2015-06-11 23:36:13 -0400

    more formatting changes

--- library_adaptor.h
+++ library_adaptor.h
@@ -9,212 +9,213 @@
 #include <cassert>
 
 namespace {
-	// helpers to fetch arguments more genrically
-	template <class T>
-	struct argument_handler {
-	};
 
-	template <>
-	struct argument_handler<int> {
-		argument_handler(quixey *engine) : engine_(engine) {
-			assert(engine);
-		}
+// helpers to fetch arguments more genrically
+template <class T>
+struct argument_handler {
+};
 
-		int operator()() const {
-			variable ret;
-			engine_->eval_exp(ret);
-			return to_integer(ret);
-		}
-	private:
-		quixey *const engine_;
-	};
-
-	template <>
-	struct argument_handler<char> {
-		argument_handler(quixey *engine) : engine_(engine) {
-			assert(engine);
-		}
-
-		char operator()() const {
-			variable ret;
-			engine_->eval_exp(ret);
-			return static_cast<char>(to_integer(ret));
-		}
-	private:
-		quixey *const engine_;
-	};
-
-	template <>
-	struct argument_handler<std::string> {
-		argument_handler(quixey *engine) : engine_(engine) {
-			assert(engine);
-		}
-
-		std::string operator()() const {
-			variable ret;
-			engine_->eval_exp(ret);
-			return to_string(ret);
-		}
-	private:
-		quixey *const engine_;
-	};
-
-	template <>
-	struct argument_handler<variable> {
-		argument_handler(quixey *engine) : engine_(engine) {
-			assert(engine);
-		}
-
-		variable operator()() const {
-			variable ret;
-			engine_->eval_exp(ret);
-			return ret;
-		}
-	private:
-		quixey *const engine_;
-	};
-
-	template <>
-	struct argument_handler<quixey *> {
-		argument_handler(quixey *engine) : engine_(engine) {
-			assert(engine);
-		}
-
-		quixey *operator()() const {
-			return engine_;
-		}
-	private:
-		quixey *const engine_;
-	};
-
-
-	template <class T>
-	void fetch_and_test_token_x(quixey *engine, token::Type expected) {
+template <>
+struct argument_handler<int> {
+	argument_handler(quixey *engine) : engine_(engine) {
 		assert(engine);
-		const token &tok = engine->get_token();
-		if(tok.type() != expected) {
-			throw T();
-		}
 	}
 
-	// processing function arguments
+	int operator()() const {
+		variable ret;
+		engine_->eval_exp(ret);
+		return to_integer(ret);
+	}
+private:
+	quixey *const engine_;
+};
 
-	template <class F, class... Args>
-	struct function_helper;
+template <>
+struct argument_handler<char> {
+	argument_handler(quixey *engine) : engine_(engine) {
+		assert(engine);
+	}
 
-	template <class F>
-	struct function_helper<F> {
-		explicit function_helper(F f) : f_(f) {
-		}
+	char operator()() const {
+		variable ret;
+		engine_->eval_exp(ret);
+		return static_cast<char>(to_integer(ret));
+	}
+private:
+	quixey *const engine_;
+};
 
-		int operator()(quixey *engine) const {
-			(void)engine;
-			assert(engine);
-			return f_();
-		}
-	private:
-		F f_;
-	};
+template <>
+struct argument_handler<std::string> {
+	argument_handler(quixey *engine) : engine_(engine) {
+		assert(engine);
+	}
 
-	template <class F, class A1>
-	struct function_helper<F, A1> {
-	public:
-		explicit function_helper(F f) : f_(f) {
-		}
+	std::string operator()() const {
+		variable ret;
+		engine_->eval_exp(ret);
+		return to_string(ret);
+	}
+private:
+	quixey *const engine_;
+};
 
-		int operator()(quixey *engine) const {
-			assert(engine);
-			const A1 arg1 = argument_handler<A1>(engine)();
-			return f_(arg1);
-		}
+template <>
+struct argument_handler<variable> {
+	argument_handler(quixey *engine) : engine_(engine) {
+		assert(engine);
+	}
 
-	private:
-		F f_;
-	};
+	variable operator()() const {
+		variable ret;
+		engine_->eval_exp(ret);
+		return ret;
+	}
+private:
+	quixey *const engine_;
+};
 
-	template <class F, class A1, class A2>
-	struct function_helper<F, A1, A2> {
-	public:
-		explicit function_helper(F f) : f_(f) {
-		}
+template <>
+struct argument_handler<quixey *> {
+	argument_handler(quixey *engine) : engine_(engine) {
+		assert(engine);
+	}
 
-		int operator()(quixey *engine) const {
+	quixey *operator()() const {
+		return engine_;
+	}
+private:
+	quixey *const engine_;
+};
 
-			assert(engine);
-			const A1 arg1 = argument_handler<A1>(engine)();
-			fetch_and_test_token_x<comma_expected>(engine, token::COMMA);
-			const A2 arg2 = argument_handler<A2>(engine)();
-			return f_(arg1, arg2);
-		}
-	private:
-		F f_;
-	};
 
-	template <class F, class A1, class A2, class A3>
-	struct function_helper<F, A1, A2, A3> {
-	public:
-		explicit function_helper(F f) : f_(f) {
-		}
+template <class T>
+void fetch_and_test_token_x(quixey *engine, token::Type expected) {
+	assert(engine);
+	const token &tok = engine->get_token();
+	if(tok.type() != expected) {
+		throw T();
+	}
+}
 
-		int operator()(quixey *engine) const {
+// processing function arguments
 
-			assert(engine);
-			const A1 arg1 = argument_handler<A1>(engine)();
-			fetch_and_test_token_x<comma_expected>(engine, token::COMMA);
-			const A2 arg2 = argument_handler<A2>(engine)();
-			fetch_and_test_token_x<comma_expected>(engine, token::COMMA);
-			const A3 arg3 = argument_handler<A3>(engine)();
-			return f_(arg1, arg2, arg3);
-		}
-	private:
-		F f_;
-	};
+template <class F, class... Args>
+struct function_helper;
 
-	template <class F, class A1, class A2, class A3, class A4>
-	struct function_helper<F, A1, A2, A3, A4> {
-	public:
-		explicit function_helper(F f) : f_(f) {
-		}
+template <class F>
+struct function_helper<F> {
+	explicit function_helper(F f) : f_(f) {
+	}
 
-		int operator()(quixey *engine) const {
+	int operator()(quixey *engine) const {
+		(void)engine;
+		assert(engine);
+		return f_();
+	}
+private:
+	F f_;
+};
 
-			assert(engine);
-			const A1 arg1 = argument_handler<A1>(engine)();
-			fetch_and_test_token_x<comma_expected>(engine, token::COMMA);
-			const A2 arg2 = argument_handler<A2>(engine)();
-			fetch_and_test_token_x<comma_expected>(engine, token::COMMA);
-			const A3 arg3 = argument_handler<A3>(engine)();
-			fetch_and_test_token_x<comma_expected>(engine, token::COMMA);
-			const A4 arg4 = argument_handler<A4>(engine)();
-			return f_(arg1, arg2, arg3, arg4);
-		}
-	private:
-		F f_;
-	};
+template <class F, class A1>
+struct function_helper<F, A1> {
+public:
+	explicit function_helper(F f) : f_(f) {
+	}
 
-	template <class F, class A1, class A2, class A3, class A4, class A5>
-	struct function_helper<F, A1, A2, A3, A4, A5> {
-	public:
-		explicit function_helper(F f) : f_(f) {
-		}
+	int operator()(quixey *engine) const {
+		assert(engine);
+		const A1 arg1 = argument_handler<A1>(engine)();
+		return f_(arg1);
+	}
 
-		int operator()(quixey *engine) const {
+private:
+	F f_;
+};
 
-			assert(engine);
-			const A1 arg1 = argument_handler<A1>(engine)();
-			fetch_and_test_token_x<comma_expected>(engine, token::COMMA);
-			const A2 arg2 = argument_handler<A2>(engine)();
-			fetch_and_test_token_x<comma_expected>(engine, token::COMMA);
-			const A3 arg3 = argument_handler<A3>(engine)();
-			fetch_and_test_token_x<comma_expected>(engine, token::COMMA);
-			const A4 arg4 = argument_handler<A4>(engine)();
-			fetch_and_test_token_x<comma_expected>(engine, token::COMMA);
-			const A5 arg5 = argument_handler<A5>(engine)();
-			return f_(arg1, arg2, arg3, arg4, arg5);
-		}
-	private:
-		F f_;
-	};
+template <class F, class A1, class A2>
+struct function_helper<F, A1, A2> {
+public:
+	explicit function_helper(F f) : f_(f) {
+	}
+
+	int operator()(quixey *engine) const {
+
+		assert(engine);
+		const A1 arg1 = argument_handler<A1>(engine)();
+		fetch_and_test_token_x<comma_expected>(engine, token::COMMA);
+		const A2 arg2 = argument_handler<A2>(engine)();
+		return f_(arg1, arg2);
+	}
+private:
+	F f_;
+};
+
+template <class F, class A1, class A2, class A3>
+struct function_helper<F, A1, A2, A3> {
+public:
+	explicit function_helper(F f) : f_(f) {
+	}
+
+	int operator()(quixey *engine) const {
+
+		assert(engine);
+		const A1 arg1 = argument_handler<A1>(engine)();
+		fetch_and_test_token_x<comma_expected>(engine, token::COMMA);
+		const A2 arg2 = argument_handler<A2>(engine)();
+		fetch_and_test_token_x<comma_expected>(engine, token::COMMA);
+		const A3 arg3 = argument_handler<A3>(engine)();
+		return f_(arg1, arg2, arg3);
+	}
+private:
+	F f_;
+};
+
+template <class F, class A1, class A2, class A3, class A4>
+struct function_helper<F, A1, A2, A3, A4> {
+public:
+	explicit function_helper(F f) : f_(f) {
+	}
+
+	int operator()(quixey *engine) const {
+
+		assert(engine);
+		const A1 arg1 = argument_handler<A1>(engine)();
+		fetch_and_test_token_x<comma_expected>(engine, token::COMMA);
+		const A2 arg2 = argument_handler<A2>(engine)();
+		fetch_and_test_token_x<comma_expected>(engine, token::COMMA);
+		const A3 arg3 = argument_handler<A3>(engine)();
+		fetch_and_test_token_x<comma_expected>(engine, token::COMMA);
+		const A4 arg4 = argument_handler<A4>(engine)();
+		return f_(arg1, arg2, arg3, arg4);
+	}
+private:
+	F f_;
+};
+
+template <class F, class A1, class A2, class A3, class A4, class A5>
+struct function_helper<F, A1, A2, A3, A4, A5> {
+public:
+	explicit function_helper(F f) : f_(f) {
+	}
+
+	int operator()(quixey *engine) const {
+
+		assert(engine);
+		const A1 arg1 = argument_handler<A1>(engine)();
+		fetch_and_test_token_x<comma_expected>(engine, token::COMMA);
+		const A2 arg2 = argument_handler<A2>(engine)();
+		fetch_and_test_token_x<comma_expected>(engine, token::COMMA);
+		const A3 arg3 = argument_handler<A3>(engine)();
+		fetch_and_test_token_x<comma_expected>(engine, token::COMMA);
+		const A4 arg4 = argument_handler<A4>(engine)();
+		fetch_and_test_token_x<comma_expected>(engine, token::COMMA);
+		const A5 arg5 = argument_handler<A5>(engine)();
+		return f_(arg1, arg2, arg3, arg4, arg5);
+	}
+private:
+	F f_;
+};
 
 }
 
