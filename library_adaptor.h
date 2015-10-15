b05ef22a264ad4e71ecd8f802567f5a99a541e84
Evan Teran <evan.teran@gmail.com>
2015-10-15 18:15:43 -0400

    through some clever template magic, there is now no upper limit to the numberof arguments that library functions can have

--- library_adaptor.h
+++ library_adaptor.h
@@ -6,18 +6,66 @@
 #include "quixey.h"
 #include <functional>
 #include <type_traits>
+#include <tuple>
 #include <cassert>
 
 namespace detail {
 
-// helpers to fetch arguments more genrically
-template <class T>
-struct argument_handler {
+template <class F, class Tuple, bool Done, size_t Total, size_t... N>
+struct call_impl {
+    static int call(F f, Tuple && t) {
+        return call_impl<F, Tuple, Total == 1 + sizeof...(N), Total, N..., sizeof...(N)>::call(f, std::forward<Tuple>(t));
+    }
 };
 
+template <class F, class Tuple, size_t Total, size_t... N>
+struct call_impl<F, Tuple, true, Total, N...> {
+    static int call(F f, Tuple && t) {
+        return f(std::get<N>(std::forward<Tuple>(t))...);
+    }
+};
+
+//-----------------------------------------------------------------------------
+// Name: call
+// Desc: calls f, with each element of the tuple t as arguments
+//-----------------------------------------------------------------------------
+template <class F, class Tuple>
+int call(F f, Tuple &&t) {
+    using tuple_type = typename std::decay<Tuple>::type;
+    return call_impl<F, Tuple, 0 == std::tuple_size<tuple_type>::value, std::tuple_size<tuple_type>::value>::call(f, std::forward<Tuple>(t));
+}
+
+//-----------------------------------------------------------------------------
+// Name: for_each
+// Desc: for_each, but for tuples (terminating case)
+//-----------------------------------------------------------------------------
+template <std::size_t I = 0, class Func, class... Tp>
+typename std::enable_if<I == sizeof...(Tp), void>::type for_each(std::tuple<Tp...> &, Func) {
+}
+
+//-----------------------------------------------------------------------------
+// Name: for_each
+// Desc: for_each, but for tuples (general case)
+//-----------------------------------------------------------------------------
+template<std::size_t I = 0, class Func, class... Tp>
+typename std::enable_if<I < sizeof...(Tp), void>::type for_each(std::tuple<Tp...>& t, Func f) {
+	f(std::get<I>(t));
+	for_each<I + 1, Func, Tp...>(t, f);
+}
+
+
+// helpers to fetch arguments more genrically
+template <class T>
+struct fetch_argument {
+};
+
+//-----------------------------------------------------------------------------
+// Name: fetch_argument
+// Desc:
+//-----------------------------------------------------------------------------
 template <>
-struct argument_handler<int> {
-	argument_handler(quixey *engine) : engine_(engine) {
+struct fetch_argument<int> {
+	fetch_argument(quixey *engine) : engine_(engine) {
 		assert(engine);
 	}
 
@@ -30,9 +78,13 @@ private:
 	quixey *const engine_;
 };
 
+//-----------------------------------------------------------------------------
+// Name: fetch_argument
+// Desc:
+//-----------------------------------------------------------------------------
 template <>
-struct argument_handler<char> {
-	argument_handler(quixey *engine) : engine_(engine) {
+struct fetch_argument<char> {
+	fetch_argument(quixey *engine) : engine_(engine) {
 		assert(engine);
 	}
 
@@ -45,9 +97,13 @@ private:
 	quixey *const engine_;
 };
 
+//-----------------------------------------------------------------------------
+// Name: fetch_argument
+// Desc:
+//-----------------------------------------------------------------------------
 template <>
-struct argument_handler<std::string> {
-	argument_handler(quixey *engine) : engine_(engine) {
+struct fetch_argument<std::string> {
+	fetch_argument(quixey *engine) : engine_(engine) {
 		assert(engine);
 	}
 
@@ -60,9 +116,13 @@ private:
 	quixey *const engine_;
 };
 
+//-----------------------------------------------------------------------------
+// Name: fetch_argument
+// Desc:
+//-----------------------------------------------------------------------------
 template <>
-struct argument_handler<variable> {
-	argument_handler(quixey *engine) : engine_(engine) {
+struct fetch_argument<variable> {
+	fetch_argument(quixey *engine) : engine_(engine) {
 		assert(engine);
 	}
 
@@ -75,9 +135,13 @@ private:
 	quixey *const engine_;
 };
 
+//-----------------------------------------------------------------------------
+// Name: fetch_argument
+// Desc:
+//-----------------------------------------------------------------------------
 template <>
-struct argument_handler<quixey *> {
-	argument_handler(quixey *engine) : engine_(engine) {
+struct fetch_argument<quixey *> {
+	fetch_argument(quixey *engine) : engine_(engine) {
 		assert(engine);
 	}
 
@@ -89,6 +153,10 @@ private:
 };
 
 
+//-----------------------------------------------------------------------------
+// Name: fetch_and_test_token_x
+// Desc: read a token and make sure it's correct
+//-----------------------------------------------------------------------------
 template <class T>
 void fetch_and_test_token_x(quixey *engine, token::Type expected) {
 	assert(engine);
@@ -98,11 +166,35 @@ void fetch_and_test_token_x(quixey *engine, token::Type expected) {
 	}
 }
 
-// processing function arguments
+//-----------------------------------------------------------------------------
+// Name: assignment_helper
+// Desc: reads a comma, then an argument of type T and assigns it to the argument
+//       wish we had generic lambdas like C++14 :-P
+//-----------------------------------------------------------------------------
+struct assignment_helper {
 
+	assignment_helper(quixey *engine) : engine_(engine) {
+	}
+
+	template <class T>
+	void operator()(T &arg) const {
+		// we want a comma, then an arg.
+		// this is used to fetch arguments 2-N
+		fetch_and_test_token_x<comma_expected>(engine_, token::COMMA);
+		arg = fetch_argument<T>(engine_)();
+	}
+private:
+	quixey *const engine_;
+};
+
+//-----------------------------------------------------------------------------
+// Name: function_helper
+// Desc: handles 0
+//-----------------------------------------------------------------------------
 template <class F, class... Args>
 struct function_helper;
 
+// no arguments
 template <class F>
 struct function_helper<F> {
 	explicit function_helper(F f) : f_(f) {
@@ -117,24 +209,12 @@ private:
 	F f_;
 };
 
-template <class F, class A1>
-struct function_helper<F, A1> {
-public:
-	explicit function_helper(F f) : f_(f) {
-	}
-
-	int operator()(quixey *engine) const {
-		assert(engine);
-		const A1 arg1 = argument_handler<A1>(engine)();
-		return f_(arg1);
-	}
-
-private:
-	F f_;
-};
-
-template <class F, class A1, class A2>
-struct function_helper<F, A1, A2> {
+//-----------------------------------------------------------------------------
+// Name: function_helper
+// Desc: handles 1 or more arguments
+//-----------------------------------------------------------------------------
+template <class F, class A1, class... A2>
+struct function_helper<F, A1, A2...> {
 public:
 	explicit function_helper(F f) : f_(f) {
 	}
@@ -142,76 +222,18 @@ public:
 	int operator()(quixey *engine) const {
 
 		assert(engine);
-		const A1 arg1 = argument_handler<A1>(engine)();
-		fetch_and_test_token_x<comma_expected>(engine, token::COMMA);
-		const A2 arg2 = argument_handler<A2>(engine)();
-		return f_(arg1, arg2);
-	}
-private:
-	F f_;
-};
 
-template <class F, class A1, class A2, class A3>
-struct function_helper<F, A1, A2, A3> {
-public:
-	explicit function_helper(F f) : f_(f) {
-	}
+		// get the first argument
+		auto arg1 = std::make_tuple(fetch_argument<A1>(engine)());
 
-	int operator()(quixey *engine) const {
+		// read the rest of the arguments
+		std::tuple<A2...> args;
+		for_each(args, assignment_helper(engine));
 
-		assert(engine);
-		const A1 arg1 = argument_handler<A1>(engine)();
-		fetch_and_test_token_x<comma_expected>(engine, token::COMMA);
-		const A2 arg2 = argument_handler<A2>(engine)();
-		fetch_and_test_token_x<comma_expected>(engine, token::COMMA);
-		const A3 arg3 = argument_handler<A3>(engine)();
-		return f_(arg1, arg2, arg3);
-	}
-private:
-	F f_;
-};
-
-template <class F, class A1, class A2, class A3, class A4>
-struct function_helper<F, A1, A2, A3, A4> {
-public:
-	explicit function_helper(F f) : f_(f) {
-	}
-
-	int operator()(quixey *engine) const {
-
-		assert(engine);
-		const A1 arg1 = argument_handler<A1>(engine)();
-		fetch_and_test_token_x<comma_expected>(engine, token::COMMA);
-		const A2 arg2 = argument_handler<A2>(engine)();
-		fetch_and_test_token_x<comma_expected>(engine, token::COMMA);
-		const A3 arg3 = argument_handler<A3>(engine)();
-		fetch_and_test_token_x<comma_expected>(engine, token::COMMA);
-		const A4 arg4 = argument_handler<A4>(engine)();
-		return f_(arg1, arg2, arg3, arg4);
-	}
-private:
-	F f_;
-};
-
-template <class F, class A1, class A2, class A3, class A4, class A5>
-struct function_helper<F, A1, A2, A3, A4, A5> {
-public:
-	explicit function_helper(F f) : f_(f) {
-	}
-
-	int operator()(quixey *engine) const {
-
-		assert(engine);
-		const A1 arg1 = argument_handler<A1>(engine)();
-		fetch_and_test_token_x<comma_expected>(engine, token::COMMA);
-		const A2 arg2 = argument_handler<A2>(engine)();
-		fetch_and_test_token_x<comma_expected>(engine, token::COMMA);
-		const A3 arg3 = argument_handler<A3>(engine)();
-		fetch_and_test_token_x<comma_expected>(engine, token::COMMA);
-		const A4 arg4 = argument_handler<A4>(engine)();
-		fetch_and_test_token_x<comma_expected>(engine, token::COMMA);
-		const A5 arg5 = argument_handler<A5>(engine)();
-		return f_(arg1, arg2, arg3, arg4, arg5);
+		// merge the two tuples into 1 and then call f_
+		// with the full tuple as arguments
+		auto all_args = std::tuple_cat(arg1, args);
+		return call(f_, all_args);
 	}
 private:
 	F f_;
@@ -219,9 +241,21 @@ private:
 
 }
 
+//-----------------------------------------------------------------------------
+// Name: wrap_function
+// Desc: works for plain old function pointers
+//-----------------------------------------------------------------------------
 template <class R, class...Args>
 std::function<int(quixey *engine)> wrap_function(R(*f)(Args...)) {
-	static_assert(sizeof...(Args) <= 5, "Function Has Too Many Arguments");
+	return detail::function_helper<decltype(f), Args...>(f);
+}
+
+//-----------------------------------------------------------------------------
+// Name: wrap_function
+// Desc: works with std::function<>
+//-----------------------------------------------------------------------------
+template <class R, class...Args>
+std::function<int(quixey *engine)> wrap_function(std::function<R(Args...)> f) {
 	return detail::function_helper<decltype(f), Args...>(f);
 }
 
